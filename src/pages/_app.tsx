b9c8a1d9d1775e9aa2f28f7e610b1a9fa7e7c9a7
Cristian Fonseca <cfonsecacomas@gmail.com>
2021-08-12 20:12:49 -0700

    Twitter Card

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,13 +13,13 @@ export default function MyApp({ Component, pageProps } : MyAppProps) {
         <meta name="twitter:site" content="cristianfonseca.com" />
         <meta name="twitter:title" content="Embed a Twitch Video into a React Website" />
         <meta name="twitter:description" content="How to embed a Twitch video into a React application or website using the ReactPlayer npm package." />
-        <meta name="twitter:image" content="images/logo2.webp" />
+        <meta name="twitter:image" content="https://www.cristianfonseca.com/images/logo2.webp" />
         <title>Cristian Fonseca</title>
         <meta name="description" content="A young developer web full-stack from Cuba"/>
         <meta property="og:title" content="Cristian Fonseca"/>
         <link rel="icon" href="/favicon.ico"/>
         <meta property="og:url" content="https://www.cristianfonseca.com" key="ogurl" />
-        <meta property="og:image" content="/images/logo2.webp" key="ogimage" />
+        <meta property="og:image" content="https://www.cristianfonseca.com/images/logo2.webp" key="ogimage" />
       </Head>
       <Component {...pageProps} />
     </>
