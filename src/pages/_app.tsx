2bfeaff9abbf1b152524a31b9d99fac0224f6446
Cristian Fonseca <cfonsecacomas@gmail.com>
2021-08-12 20:14:44 -0700

    Twitter card changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,8 +11,8 @@ export default function MyApp({ Component, pageProps } : MyAppProps) {
       <Head>
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="cristianfonseca.com" />
-        <meta name="twitter:title" content="Embed a Twitch Video into a React Website" />
-        <meta name="twitter:description" content="How to embed a Twitch video into a React application or website using the ReactPlayer npm package." />
+        <meta name="twitter:title" content="Cristian Fonseca" />
+        <meta name="twitter:description" content="A young developer web full-stack from Cuba" />
         <meta name="twitter:image" content="https://www.cristianfonseca.com/images/logo2.webp" />
         <title>Cristian Fonseca</title>
         <meta name="description" content="A young developer web full-stack from Cuba"/>
