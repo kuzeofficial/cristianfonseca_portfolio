fe8eae886e1a3aa7fec83aa58a8d23391def68c5
Cristian Fonseca <cfonsecacomas@gmail.com>
2021-08-12 20:05:06 -0700

    Meta Twitter Card

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,6 +9,11 @@ export default function MyApp({ Component, pageProps } : MyAppProps) {
   return (
     <>
       <Head>
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="cristianfonseca.com" />
+        <meta name="twitter:title" content="Embed a Twitch Video into a React Website" />
+        <meta name="twitter:description" content="How to embed a Twitch video into a React application or website using the ReactPlayer npm package." />
+        <meta name="twitter:image" content="logo2.webp" />
         <title>Cristian Fonseca</title>
         <meta name="description" content="A young developer web full-stack from Cuba"/>
         <meta property="og:title" content="Cristian Fonseca"/>
