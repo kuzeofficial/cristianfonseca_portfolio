0a0fcac545c834befa32c51df7e7cf755d46f77b
Cristian Fonseca <cfonsecacomas@gmail.com>
2021-08-12 19:37:40 -0700

    META Changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,10 +10,12 @@ export default function MyApp({ Component, pageProps } : MyAppProps) {
     <>
       <Head>
         <title>Cristian Fonseca - Web Developer</title>
+        <meta name="description" content="Full-stack web developer"/>
+        <meta property="og:title" content="Cristian Fonseca"/>
+        <link rel="icon" href="/favicon.ico"/>
         <meta property="og:url" content="https://www.cristianfonseca.com" key="ogurl" />
         <meta property="og:image" content="/images/logo2.webp" key="ogimage" />
         <meta property="og:site_name" content="Cristian Fonseca" key="ogsitename" />
-        <meta property="og:title" content="Cristian Fonseca" key="ogtitle" />
         <meta property="og:description" content="Full-stack web developer" key="ogdesc" />
       </Head>
       <Component {...pageProps} />
