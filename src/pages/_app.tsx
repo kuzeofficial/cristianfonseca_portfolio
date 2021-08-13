67bf9d8397781ab42072b4781f67a723f20f0fae
Cristian Fonseca <cfonsecacomas@gmail.com>
2021-08-12 20:09:23 -0700

    Image meta tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,7 +13,7 @@ export default function MyApp({ Component, pageProps } : MyAppProps) {
         <meta name="twitter:site" content="cristianfonseca.com" />
         <meta name="twitter:title" content="Embed a Twitch Video into a React Website" />
         <meta name="twitter:description" content="How to embed a Twitch video into a React application or website using the ReactPlayer npm package." />
-        <meta name="twitter:image" content="logo2.webp" />
+        <meta name="twitter:image" content="images/logo2.webp" />
         <title>Cristian Fonseca</title>
         <meta name="description" content="A young developer web full-stack from Cuba"/>
         <meta property="og:title" content="Cristian Fonseca"/>
