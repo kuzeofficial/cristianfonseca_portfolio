ba817f125fb8bf31bcd3646fcc287dbb651daa8c
Cristian Fonseca <cfonsecacomas@gmail.com>
2021-09-12 22:05:29 -0400

    Fixed type in props

--- src/components/MDXComponents.tsx
+++ src/components/MDXComponents.tsx
@@ -3,9 +3,9 @@ import { CustomImage } from './MD/CustomImage'
 import Image from "next/image";
 
 export const MDXComponents = {
-    h1: (props) => <h1 className="textblock tracking-wide theme-dark bg-clip-text items-center text-4xl lg:text-4xl md:text-3xl sm:text-3xl mt-5" {...props}/>,
-    img: (props) => <CustomImage {...props} className="rounded-md "/>,
-    a: (props) => <a className="text-transparent theme-gradient bg-clip-text font-bold" {...props}/>,
-    p: (props) => <p className="font-light mt-5 text-md font-normal max-h-3xl  block overflow-ellipsis overflow-hidden break-words mb-2" {...props}/>,
+    h1: (props:any) => <h1 className="textblock tracking-wide theme-dark bg-clip-text items-center text-4xl lg:text-4xl md:text-3xl sm:text-3xl mt-5" {...props}/>,
+    img: (props:any) => <CustomImage {...props} className="rounded-md "/>,
+    a: (props:any) => <a className="text-transparent theme-gradient bg-clip-text font-bold" {...props}/>,
+    p: (props:any) => <p className="font-light mt-5 text-md font-normal max-h-3xl  block overflow-ellipsis overflow-hidden break-words mb-2" {...props}/>,
     Image,
 }
