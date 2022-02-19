09844a2f511b67cb9f919be706c5a62f3b8abcb3
Cristian Fonseca <cfonsecacomas@gmail.com>
2022-02-19 00:37:03 -0300

    Modify ui and new profile image

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,18 +6,18 @@ export const Hero = () => {
   return (
     <div className="flex flex-col justify-between my-10 space-y-14 md:space-y-0 lg:flex-row">
       <div className="flex flex-col">
-        <div className="flex flex-col md:flex-row items-center">
+        <div className="flex flex-col items-center md:flex-row">
           <Image
-            src="/images/logo2.webp"
+            src="/images/logo.webp"
             alt="Cristian Fonseca"
             width={144}
             height={154}
-            className="shadow-lg rounded-md border-none"
+            className="border-none rounded-2xl"
             placeholder="blur"
-            blurDataURL="/images/logo2.webp"
+            blurDataURL="/images/logo.webp"
           />
-          <div className="ml-3 flex flex-col md:items-start items-center">
-            <h1 className="flex flex-col sm:flex-row items-center text-4xl lg:text-4xl md:text-3xl sm:text-3xl text-center">
+          <div className="flex flex-col items-center ml-3 md:items-start">
+            <h1 className="flex flex-col items-center text-4xl text-center sm:flex-row lg:text-4xl md:text-3xl sm:text-3xl">
               <span className="sm:ml-2">Hi, I'm </span>
               <Image
                 className="flex flex-row"
@@ -26,11 +26,11 @@ export const Hero = () => {
                 width="40px"
                 height="40px"
               />
-              <span className="block tracking-wide ml-2 text-transparent theme-gradient bg-clip-text whitespace-nowrap md:inline">
+              <span className="block ml-2 tracking-wide text-transparent theme-gradient bg-clip-text whitespace-nowrap md:inline">
                 Cristian Fonseca
               </span>
             </h1>
-            <h2 className="font-light  max-w-lg mt-4 text-center md:text-left sm:text-left text-1xl sm:text-2xl md:text-1xl">
+            <h2 className="max-w-lg mt-4 font-light text-center md:text-left sm:text-left text-1xl sm:text-2xl md:text-1xl">
               A young web developer🤓 passionate about technology💻 and
               communication🛰️ from Brasil🏝️
             </h2>
@@ -39,7 +39,7 @@ export const Hero = () => {
 
         <Skills />
       </div>
-      <div className="flex flex-col items-start  justify-center space-y-2">
+      <div className="flex flex-col items-start justify-center space-y-2">
         <a
           className="text-lg font-semibold transition-colors text-primary hover:text-secondary group"
           href="/Cristian Fonseca.pdf"
--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,12 +1,15 @@
-export const Section = (props:any) => {
-    return (
-        <div id={props.id} className={`w-full py-12 leading-loose text-muted ${props.className}`}>
-            <div className="max-w-6xl mx-auto">
-                <h2 className="mb-2 text-3xl font-semibold text-left text-primary">
-                    {props.title}
-                </h2>
-                {props.children}
-            </div>
-        </div>
-    )
-}
+export const Section = (props: any) => {
+  return (
+    <div
+      id={props.id}
+      className={`w-full py-12 leading-loose text-muted ${props.className}`}
+    >
+      <div className="max-w-6xl mx-auto">
+        <h2 className="mb-4 text-3xl font-semibold text-center text-primary">
+          {props.title}
+        </h2>
+        {props.children}
+      </div>
+    </div>
+  );
+};
